e38ddf989aa5708cf2795dc0d235931749b78525
RLN <113087232+RLN2004@users.noreply.github.com>
2023-06-14 13:18:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+/function noteFunction() {
+    location.href = "note.html";
+}/
+
+function myfunc1(){
+    document.getElementById("myForm").style.display = "block";
+}
+function killFunc(){
+    location.href = "done.html";
+}
